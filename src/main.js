c9dbd6a8e61b4ebb8e7f9bd8ee0d04f21b2e90c0
JP27 <joanpomaresherrero@gmail.com>
2026-03-13 16:18:34 +0100

    cambios modo oscur

--- src/main.js
+++ src/main.js
@@ -77,3 +77,29 @@ import './style.css'
                 duration: 3,
        });
       
+let theme = localStorage.getItem('theme');
+const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches
+
+// Comprueba si ha sido la primera vez que el usuario entra a la pagina que estilo debe tener la pagina
+if(!theme && isDark) {
+    theme = 'dark'
+}
+else if(!theme) {
+    theme = 'light'
+}
+
+// Si el tema del usuario es oscuro la pagina se vuelve modo oscuro
+if(theme == 'dark') { document.documentElement.classList.add('dark') }
+
+// Boton de cambio de tema
+const themeSwitch = document.getElementById('theme-switch');
+      
+// Listener de click para el boton del cambio de tema que cambia y guarda el estilo de la pagina
+themeSwitch.addEventListener('click', function() {
+    document.documentElement.classList.toggle('dark'); 
+
+    if(theme && theme == 'dark') { localStorage.setItem('theme', 'light') }
+    else if(theme) { localStorage.setItem('theme', 'dark') }
+})
+
+// localStorage.removeItem('theme') // Solo para probar si se pone el estilo por defecto del usuario
