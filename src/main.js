78fb9a38f01ce7dd06271485bb4d8f7257a7a1ce
HugoLG <hugolaorden7@gmail.com>
2026-03-22 14:00:54 +0100

    timeline

--- src/main.js
+++ src/main.js
@@ -77,16 +77,16 @@ import './style.css'
             duration: 3,
         });
 
-        gsap.to('.btn-cambiar-tema', {
-            x: 200, 
-            rotation: 360,
-            scrollTrigger: {
-                trigger: '.btn-cambiar-tema',
-                start: 700,
-                end: 1000,
-                scrub: true
-            }
-        });
+        // gsap.to('.btn-cambiar-tema', {
+        //     x: 200, 
+        //     rotation: 360,
+        //     scrollTrigger: {
+        //         trigger: '.btn-cambiar-tema',
+        //         start: 700,
+        //         end: 1000,
+        //         scrub: true
+        //     }
+        // });
       
 console.log('a')
 let theme = localStorage.getItem('theme');
@@ -115,3 +115,154 @@ themeSwitch.addEventListener('click', function() {
 })
 
 // localStorage.removeItem('theme') // Solo para probar si se pone el estilo por defecto del usuario
+
+    //     gsap.to('.pagina-2', {
+    //         scrollTrigger: {
+    //             trigger: '.pagina-2',
+    //             pin:true,                
+    //             start: 'top top',
+    //             end: '+=2000',
+    //         }
+    //     })
+    //     gsap.fromTo('.figma-logo', {
+    //         width: 300,
+    //     },{
+    //         width: 100,
+    //         scrollTrigger: {
+    //             trigger: '.figma-logo',
+    //             start: '+=300',
+    //             end: '+=1000',
+    //             toggleActions: "restart pause reverse pause"
+    //         }
+    //     })
+
+    //     gsap.fromTo('.figma-title', {
+    //         opacity: 0,
+    //         display: 'none'
+    //     },{
+    //         opacity: 1,
+    //         display: 'block',
+    //         scrollTrigger: {
+            
+    //             trigger: '.figma-title',
+    //             start: '+=300',
+    //             end: '+=1000',
+    //             toggleActions: "restart pause reverse pause"
+    //         }
+    //     })
+
+    // gsap.fromTo('.figma-caja', {
+    //         opacity: 0,
+    //         display: 'none'
+    //     },{
+    //         opacity: 1,
+    //         display: 'block',
+    //         scrollTrigger: {
+            
+    //             trigger: '.figma-caja',
+    //             start: '+=600',
+    //             end: '+=1000',
+    //             toggleActions: "restart pause reverse pause"
+    //         }
+    //     })
+    
+    
+//-------------Pagina 1-------------
+let tlPagina1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.pagina-1',
+    start: 'top top',
+    end: '+=500',
+    scrub: true,
+    pin: true
+  }
+});
+
+tlPagina1.from('.inicio-descripcion', {
+    opacity: 0,
+    y: 50
+});
+
+tlPagina1.to('.btn-cambiar-tema', {
+x: 200,
+rotation: 360
+});
+
+let tpl = gsap.timeline({
+  repeat: -1
+});
+
+tpl.to('.giro', {
+  rotation: 360,
+  duration: 2,
+  ease: "linear"
+});
+
+//-------------Pagina 1-------------
+
+
+    
+let tlPagina2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.pagina-2',
+    start: 'top top',
+    end: '+=1000',
+    scrub: true,
+    pin: true
+  }
+});
+
+tlPagina2.fromTo('.figma-logo', {
+  scale: 1.5
+}, {
+  scale: 0.6,
+  duration: 1
+});
+
+tlPagina2.from('.figma-title', {
+  opacity: 0, //no se ve al principio
+  y: 50,
+  duration: 1
+}, "-=0.5"); //solapa con el siguiente
+
+tlPagina2.from('.figma-descripcion', {
+  opacity: 0,
+  x: 100,
+  duration: 1.5
+}, "+=1");
+
+tlPagina2.from('.figma-caja', {
+  opacity: 0,
+  y: 100,
+  stagger: 0.3,
+  duration: 1.5
+}, "+=1");
+
+tlPagina2.to('.figma-cajas', {
+  x: -1500,
+  duration: 2
+});
+
+tlPagina2.to('.figma-caja', {
+ opacity: 0,
+  y: 100,
+  stagger: 0.3,
+  duration: 1.5
+});
+
+        
+
+        let tlPagina3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.pagina-3',
+    start: 'top top',
+    end: '+=1000',
+    scrub: true,
+    pin: true
+            }
+        });
+tlPagina3.from('.descripcion', {
+  opacity: 0,
+  x: 100,
+  duration: 1.5
+}, "+=1");
