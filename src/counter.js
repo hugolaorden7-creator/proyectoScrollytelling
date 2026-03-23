af1eccb9f4547ef39beb5a656a65860b21890416
JP27 <joanpomaresherrero@gmail.com>
2026-03-23 10:35:11 +0100

    cambios Joan

--- src/counter.js
+++ src/counter.js
@@ -1,9 +1,154 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+import './style.css'
+
+gsap.registerPlugin(SplitText);
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.figma', {
+  y: 200
+})
+
+
+let tlPagina2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.pagina-2',
+    start: 'top top',
+    end: '+=10000',
+    scrub: true,
+    pin: true
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+});
+
+tlPagina2.from('.figma-logo', {
+  y: -100,
+  opacity: 0
+})
+
+tlPagina2.to('.figma-logo', {
+  width: 100,
+})
+
+tlPagina2.fromTo('.figma-title', {
+    opacity: 0,
+    display: 'none'
+},{
+    opacity: 1,
+    display: 'block',
+})
+
+tlPagina2.to('.figma', {
+  y: -50, 
+})
+
+tlPagina2.fromTo('.figma-descripcion', {
+    display: 'none',
+    opacity: 0
+},{
+    display: 'block',
+    opacity: 1,
+},  "-=0.5")
+
+tlPagina2.to('.figma-descripcion', {
+    display: 'none',
+    opacity: 0,
+});
+
+tlPagina2.to('.figma', {
+    y: -100,
+})
+
+tlPagina2.from('.figma-separador', {
+  opacity: 0,
+  display: 'none'
+})
+
+const tituloPaginaSplit = SplitText.create('.titulo-pagina-2', {type: 'chars'})
+
+tlPagina2.from(tituloPaginaSplit.chars, {
+  display: 'none',
+  opacity: 0,
+  stagger: 0.05,
+  x:-100
+});
+
+
+tlPagina2.from('.figma-caja', {
+  opacity: 0,
+  y: 100,
+  stagger: 0.3,
+  duration: 1.5
+}, "+=1");
+//primera accion
+
+tlPagina2.to('.figma-caja-2 ', {
+  scale: 0.8,
+  duration: 1.5
+}, "+=0.5");
+
+tlPagina2.to('.figma-caja-3', {
+  scale: 0.8,
+  duration: 1.5
+}, "<");
+
+tlPagina2.to('.figma-caja-1', {
+  scale: 1.1,
+  duration: 1
+}, "<"); 
+//Segunda accion
+tlPagina2.to('.figma-caja-1', {
+  scale: 0.8,
+  duration: 1.5
+}, "+=0.5");
+
+tlPagina2.to('.figma-caja-2', {
+  scale: 1.1,
+  duration: 1
+}, "<"); 
+//tercera accion
+tlPagina2.to('.figma-caja-2', {
+  scale: 0.8,
+  duration: 1.5
+}, "+=0.5");
+
+tlPagina2.to('.figma-caja-3', {
+  scale: 1.1,
+  duration: 1
+}, "<"); 
+
+
+//FINAL
+tlPagina2.to('.figma-caja', {
+  scale: 1,
+  duration: 2
+}, "+=1");
+
+tlPagina2.to('.figma-cajas', {
+  opacity: 0,
+  scale: 0.5,
+  duration: 2
+}, "+=1");
+
+
+let theme = localStorage.getItem('theme');
+const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches
+
+// Comprueba si ha sido la primera vez que el usuario entra a la pagina que estilo debe tener la pagina
+if(!theme && isDark) {
+    theme = 'dark'
 }
+else if(!theme) {
+    theme = 'light'
+}
+
+// Si el tema del usuario es oscuro la pagina se vuelve modo oscuro
+if(theme == 'dark') { document.documentElement.classList.add('dark') }
+
+// Boton de cambio de tema
+const themeSwitch = document.getElementById('theme-switch');
+      
+// Listener de click para el boton del cambio de tema que cambia y guarda el estilo de la pagina
+themeSwitch.addEventListener('click', function() {
+    document.documentElement.classList.toggle('dark'); 
+
+    if(theme && theme == 'dark') { localStorage.setItem('theme', 'light') }
+    else if(theme) { localStorage.setItem('theme', 'dark') }
+})
